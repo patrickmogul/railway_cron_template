72c0bef6de64b0ae82136008520b9e1fbb9727b9
patrickmogul <patrick@easynode.pro>
2024-12-02 13:10:07 -0600

    CRON ping to url.

---
+++ app.py
@@ -0,0 +1,14 @@
+import requests
+import os
+
+def notify():
+    url = os.environ['NOTIFY_URL']
+    message = "Hi"
+    response = requests.post(url, data=message)
+    if response.status_code == 200:
+        print("Notification sent successfully")
+    else:
+        print("Error sending notification")
+
+if __name__ == "__main__":
+    notify()
